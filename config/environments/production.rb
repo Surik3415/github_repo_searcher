be297f09281bb4661a47a048fd157ef79d5eda44
surik3415 <suren_avakian@epam.com>
2023-12-05 03:26:17 +0200

    config bin and puma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,7 @@ Rails.application.configure do
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
