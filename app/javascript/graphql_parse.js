e55e2186c56343144c6964a8e78073429eb2e505
surik3415 <suren_avakian@epam.com>
2023-12-05 15:13:09 +0200

    add full text of message for debuging

--- app/javascript/graphql_parse.js
+++ app/javascript/graphql_parse.js
@@ -1,6 +1,6 @@
 async function parseResponse(response) {
   if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}\n ${response}`);
+    throw new Error(`HTTP error! Status: ${response.status}\n ${new JSONObject(response)}`);
   }
 
   return response.headers.get('content-type').includes('application/json') ? await response.json() : await response.text();
